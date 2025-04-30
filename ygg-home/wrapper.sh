8e93d2686b89d439e5dc6b1d78e40d61a052c901
eebedav <ebertek@gmail.com>
2025-04-30 15:55:56 +0200

    Update wrapper.sh

--- ygg-home/wrapper.sh
+++ ygg-home/wrapper.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+set -e
+
+# Start both scripts in background
 python /app/hass.py &
-python /app/bjornify.py
+python /app/bjornify.py &
+
+# Wait for any of them to exit
 wait -n
+
+# Exit with the status of the first one that finishes
 exit $?
