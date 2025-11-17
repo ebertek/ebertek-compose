12cdbcf11bbf3d7cee69f586d285ab36f45eba02
eebedav <ebertek@gmail.com>
2025-11-17 12:26:34 +0100

    Lint

--- Scripts/acmesh/30-vpc.sh
+++ Scripts/acmesh/30-vpc.sh
@@ -16,24 +16,27 @@ while IFS='=' read -r key value; do
 done <"$ENV_FILE"
 
 scp -r -P "$PORT" -i /var/services/homes/Hannibal/.ssh/id_rsa /volume2/docker/acmesh "$USER"@"$HOST":~/
-ssh "$HOST" -i /var/services/homes/Hannibal/.ssh/id_rsa -l "$USER" -p "$PORT" <<EOF
-	sudo -s
-	rm -r /mnt/data/tntphoto_certbot_config/_data/archive/*
-	cp -R /root/acmesh/* /mnt/data/tntphoto_certbot_config/_data/archive/
-	rm -r /root/acmesh
-	mv /mnt/data/tntphoto_certbot_config/_data/archive/ebertek.com_ecc /mnt/data/tntphoto_certbot_config/_data/archive/ebertek.com
-	mv /mnt/data/tntphoto_certbot_config/_data/archive/melindaban.com_ecc /mnt/data/tntphoto_certbot_config/_data/archive/melindaban.com
-	mv /mnt/data/tntphoto_certbot_config/_data/archive/tnt.photo_ecc /mnt/data/tntphoto_certbot_config/_data/archive/tnt.photo
-	mv /mnt/data/tntphoto_certbot_config/_data/archive/ld25.se_ecc /mnt/data/tntphoto_certbot_config/_data/archive/ld25.se
-	chmod 644 /mnt/data/tntphoto_certbot_config/_data/archive/ebertek.com/*
-	chmod 644 /mnt/data/tntphoto_certbot_config/_data/archive/melindaban.com/*
-	chmod 644 /mnt/data/tntphoto_certbot_config/_data/archive/tnt.photo/*
-	chmod 644 /mnt/data/tntphoto_certbot_config/_data/archive/ld25.se/*
-	chmod 600 /mnt/data/tntphoto_certbot_config/_data/archive/ebertek.com/ebertek.com.key
-	chmod 600 /mnt/data/tntphoto_certbot_config/_data/archive/melindaban.com/melindaban.com.key
-	chmod 600 /mnt/data/tntphoto_certbot_config/_data/archive/tnt.photo/tnt.photo.key
-	chmod 600 /mnt/data/tntphoto_certbot_config/_data/archive/ld25.se/ld25.se.key
-	chown -R 101:101 /mnt/data/tntphoto_certbot_config/_data/archive
+ssh "$HOST" -i /var/services/homes/Hannibal/.ssh/id_rsa -l "$USER" -p "$PORT" <<'EOF'
+	sudo rm -r /mnt/data/tntphoto_certbot_config/_data/archive/*
+	sudo cp -R /root/acmesh/* /mnt/data/tntphoto_certbot_config/_data/archive/
+	sudo rm -r /root/acmesh
+	sudo mv /mnt/data/tntphoto_certbot_config/_data/archive/ebertek.com_ecc /mnt/data/tntphoto_certbot_config/_data/archive/ebertek.com
+	sudo mv /mnt/data/tntphoto_certbot_config/_data/archive/melindaban.com_ecc /mnt/data/tntphoto_certbot_config/_data/archive/melindaban.com
+	sudo mv /mnt/data/tntphoto_certbot_config/_data/archive/tnt.photo_ecc /mnt/data/tntphoto_certbot_config/_data/archive/tnt.photo
+	sudo mv /mnt/data/tntphoto_certbot_config/_data/archive/ld25.se_ecc /mnt/data/tntphoto_certbot_config/_data/archive/ld25.se
+	sudo chmod 644 /mnt/data/tntphoto_certbot_config/_data/archive/ebertek.com/*
+	sudo chmod 644 /mnt/data/tntphoto_certbot_config/_data/archive/melindaban.com/*
+	sudo chmod 644 /mnt/data/tntphoto_certbot_config/_data/archive/tnt.photo/*
+	sudo chmod 644 /mnt/data/tntphoto_certbot_config/_data/archive/ld25.se/*
+	sudo chmod 600 /mnt/data/tntphoto_certbot_config/_data/archive/ebertek.com/ebertek.com.key
+	sudo chmod 600 /mnt/data/tntphoto_certbot_config/_data/archive/melindaban.com/melindaban.com.key
+	sudo chmod 600 /mnt/data/tntphoto_certbot_config/_data/archive/tnt.photo/tnt.photo.key
+	sudo chmod 600 /mnt/data/tntphoto_certbot_config/_data/archive/ld25.se/ld25.se.key
+	sudo chown -R 101:101 /mnt/data/tntphoto_certbot_config/_data/archive
 	NGINX_CONTAINER=$(docker ps --filter "label=com.docker.compose.service=nginx" --format '{{.ID}}' | head -n1)
-	docker exec "$NGINX_CONTAINER" sh -c "/usr/sbin/nginx -s reload"
+	if [ -z "$NGINX_CONTAINER" ]; then
+		echo "Error: No running container found for service nginx" >&2
+		exit 1
+	fi
+	sudo docker exec "$NGINX_CONTAINER" sh -c "/usr/sbin/nginx -s reload"
 EOF
