f14c64804d10aaad58a83ef61b14f786a3f18c39
eebedav <ebertek@gmail.com>
2025-11-20 16:15:37 +0100

    Switch domains

--- Scripts/acmesh/10-acmesh.sh
+++ Scripts/acmesh/10-acmesh.sh
@@ -17,8 +17,10 @@ while IFS='=' read -r key value; do
 	export "$key=$value"
 done <"$ENV_FILE"
 
+docker exec "$ACMESH_CONTAINER" --issue -d ebi.nu -d '*.ebi.nu' -d 'ygg.nu' -d '*.ygg.nu' --server letsencrypt --dns dns_cf --force
 docker exec "$ACMESH_CONTAINER" --issue -d tnt.photo -d '*.tnt.photo' -d '*.int.tnt.photo' -d '*.ext.tnt.photo' --challenge-alias 'tnt.photo' --server letsencrypt --dns dns_cf --force
 docker exec "$ACMESH_CONTAINER" --issue -d melindaban.com -d '*.melindaban.com' -d linda-ebert.com -d '*.linda-ebert.com' --server letsencrypt --dns dns_cf --force
 docker exec "$ACMESH_CONTAINER" --issue -d ebertek.com -d '*.ebertek.com' --server letsencrypt --dns dns_cf --force
 docker exec "$ACMESH_CONTAINER" --issue -d ld25.se -d '*.ld25.se' -d lindi-david.se -d '*.lindi-david.se' --server letsencrypt --dns dns_cf --force
+docker exec "$ACMESH_CONTAINER" --toPkcs -d ebi.nu --password "$PASSWORD"
 docker exec "$ACMESH_CONTAINER" --toPkcs -d tnt.photo --password "$PASSWORD"
