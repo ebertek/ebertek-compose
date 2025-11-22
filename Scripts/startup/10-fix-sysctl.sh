7cb8f52699309594c75628f25b5c8567a73ea6f0
David Ebert <ebertek@gmail.com>
2025-11-22 21:19:47 +0100

    Change backup location

--- Scripts/startup/10-fix-sysctl.sh
+++ Scripts/startup/10-fix-sysctl.sh
@@ -13,5 +13,5 @@ sysctl -w net.ipv4.ping_group_range="0 65535"
 sysctl -w net.ipv6.conf.all.accept_ra=2
 sysctl -w net.ipv6.conf.default.accept_ra=2
 sysctl -w vm.overcommit_memory=1
-bash /volume2/docker/synology-docker/syno_docker_update.sh update -f
+bash /volume2/docker/synology-docker/syno_docker_update.sh update -f -p /volume2/docker/_backup
 bash /volume1/homes/Hannibal/Synology_HDD_db-main/syno_hdd_db.sh -nr
