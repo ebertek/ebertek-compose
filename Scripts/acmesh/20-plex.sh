0348a5419093c13eac0eee372e0ad006dbb7d8ac
David Ebert <ebertek@gmail.com>
2025-11-22 10:13:01 +0100

    Switch domain

--- Scripts/acmesh/20-plex.sh
+++ Scripts/acmesh/20-plex.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 PLEX_CONTAINER=$(docker ps --filter "label=com.docker.compose.service=plex" --format '{{.ID}}' | head -n1)
 
-rm /volume2/docker/plex/config/tnt.photo.pfx
-cp /volume2/docker/acmesh/tnt.photo_ecc/tnt.photo.pfx /volume2/docker/plex/config/tnt.photo.pfx
-chown docker:users /volume2/docker/plex/config/tnt.photo.pfx
-chmod 644 /volume2/docker/plex/config/tnt.photo.pfx
+rm /volume2/docker/plex/config/ebi.nu.pfx
+cp /volume2/docker/acmesh/ebi.nu_ecc/ebi.nu.pfx /volume2/docker/plex/config/ebi.nu.pfx
+chown docker:users /volume2/docker/plex/config/ebi.nu.pfx
+chmod 644 /volume2/docker/plex/config/ebi.nu.pfx
 docker exec "$PLEX_CONTAINER" sh -c "kill -9 \`pidof \"Plex Media Server\"\`"
--- Scripts/acmesh/50-hass.sh
+++ Scripts/acmesh/50-hass.sh
@@ -16,19 +16,19 @@ while IFS='=' read -r key value; do
 done <"$ENV_FILE"
 
 rm /volume2/docker/hass/config/ssl/fullchain.cer
-rm /volume2/docker/hass/config/ssl/tnt.photo.key
-cp /volume2/docker/acmesh/tnt.photo_ecc/fullchain.cer /volume2/docker/hass/config/ssl/fullchain.cer
-cp /volume2/docker/acmesh/tnt.photo_ecc/tnt.photo.key /volume2/docker/hass/config/ssl/tnt.photo.key
+rm /volume2/docker/hass/config/ssl/ebi.nu.key
+cp /volume2/docker/acmesh/ebi.nu_ecc/fullchain.cer /volume2/docker/hass/config/ssl/fullchain.cer
+cp /volume2/docker/acmesh/ebi.nu_ecc/ebi.nu.key /volume2/docker/hass/config/ssl/ebi.nu.key
 chmod 644 /volume2/docker/hass/config/ssl/*
 curl -k -X POST \
 	-H "Authorization: Bearer $AUTHORIZATION_TOKEN" \
 	-H "Content-Type: application/json" \
 	-d '{}' \
-	"https://hass.int.tnt.photo/api/services/homeassistant/reload_core_config"
+	"https://hass.ebi.nu/api/services/homeassistant/reload_core_config"
 
 rm /volume2/docker/hass/mosquitto/config/fullchain.pem
 rm /volume2/docker/hass/mosquitto/config/privkey.pem
-cp /volume2/docker/acmesh/tnt.photo_ecc/fullchain.cer /volume2/docker/hass/mosquitto/config/fullchain.pem
-cp /volume2/docker/acmesh/tnt.photo_ecc/tnt.photo.key /volume2/docker/hass/mosquitto/config/privkey.pem
+cp /volume2/docker/acmesh/ebi.nu_ecc/fullchain.cer /volume2/docker/hass/mosquitto/config/fullchain.pem
+cp /volume2/docker/acmesh/ebi.nu_ecc/ebi.nu.key /volume2/docker/hass/mosquitto/config/privkey.pem
 chmod 644 /volume2/docker/hass/mosquitto/config/*.pem
 chown 1883:1883 /volume2/docker/hass/mosquitto/config/*.pem
