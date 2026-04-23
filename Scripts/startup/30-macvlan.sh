a166ca36a3001afd2d4b125d873ac41d473f8f1b
eebedav <ebertek@gmail.com>
2026-04-23 09:59:22 +0200

    Add IPv6

--- Scripts/startup/30-macvlan.sh
+++ Scripts/startup/30-macvlan.sh
@@ -3,7 +3,12 @@ set -euo pipefail
 
 IFACE="macvlan1"
 PARENT="ovs_bond0"
-ADDR4="10.4.21.1/24"
+
+HOST_ADDR4="10.4.21.1/32"
+SUBNET4="10.4.20.0/23"
+
+HOST_ADDR6="2001:9b1:25fc:6900::ffff/128"
+SUBNET6="2001:9b1:25fc:6900::/64"
 
 # Wait for parent interface to exist and be up (timeout: 30s)
 for _ in {1..30}; do
@@ -21,8 +26,13 @@ if ! ip link show "$IFACE" &>/dev/null; then
 	ip link add "$IFACE" link "$PARENT" type macvlan mode bridge
 fi
 
-# Ensure the IPv4 address is present
-ip -4 addr flush dev "$IFACE"
-ip addr add "$ADDR4" dev "$IFACE"
-
+# Ensure the IP addresses are present
 ip link set "$IFACE" up
+
+ip -4 addr flush dev "$IFACE"
+ip -4 addr add "$HOST_ADDR4" dev "$IFACE"
+ip -4 route replace "$SUBNET4" dev "$IFACE"
+
+ip -6 addr flush dev "$IFACE"
+ip -6 addr add "$HOST_ADDR6" dev "$IFACE"
+ip -6 route replace "$SUBNET6" dev "$IFACE"
