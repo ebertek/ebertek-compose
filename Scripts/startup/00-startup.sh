71a5120c06485268590f6b142b5c7beac304638f
eebedav <ebertek@gmail.com>
2026-05-06 15:41:28 +0200

    Clean up

--- Scripts/startup/00-startup.sh
+++ Scripts/startup/00-startup.sh
@@ -6,3 +6,6 @@ sh /volume2/docker/ebertek-compose/Scripts/startup/40-disable-active_insight.sh
 bash /volume2/docker/ebertek-compose/Scripts/startup/50-sdp.sh
 bash /volume2/docker/ebertek-compose/Scripts/startup/60-rclone.sh
 bash /volume2/docker/ebertek-compose/Scripts/startup/70-youtube.sh
+cd /volume2/docker/_backup/
+bash /volume2/docker/synology-docker/syno_docker_update.sh update -f -p /volume2/docker/_backup
+bash /volume1/homes/Hannibal/Synology_HDD_db-main/syno_hdd_db.sh -nr
