990770fc68931ea583450a5986deb3c1fdfa837c
eebedav <ebertek@gmail.com>
2026-05-06 15:49:45 +0200

    Lint

--- Scripts/startup/00-startup.sh
+++ Scripts/startup/00-startup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-bash /volume2/docker/ebertek-compose/Scripts/startup/10-fix-sysctl.sh
-sh /volume2/docker/ebertek-compose/Scripts/startup/20-insmod-tun.sh
-bash /volume2/docker/ebertek-compose/Scripts/startup/30-macvlan.sh
-sh /volume2/docker/ebertek-compose/Scripts/startup/40-disable-active_insight.sh
-bash /volume2/docker/ebertek-compose/Scripts/startup/50-sdp.sh
-bash /volume2/docker/ebertek-compose/Scripts/startup/60-rclone.sh
-bash /volume2/docker/ebertek-compose/Scripts/startup/70-youtube.sh
-cd /volume2/docker/_backup/
-bash /volume2/docker/synology-docker/syno_docker_update.sh update -f -p /volume2/docker/_backup
-bash /volume1/homes/Hannibal/Synology_HDD_db-main/syno_hdd_db.sh -nr
+
+run() {
+	echo "[$(date '+%F %T')] Running: $*"
+	"$@" || echo "[$(date '+%F %T')] FAILED: $*" >&2
+}
+
+run bash /volume2/docker/ebertek-compose/Scripts/startup/10-fix-sysctl.sh
+run sh /volume2/docker/ebertek-compose/Scripts/startup/20-insmod-tun.sh
+run bash /volume2/docker/ebertek-compose/Scripts/startup/30-macvlan.sh
+run sh /volume2/docker/ebertek-compose/Scripts/startup/40-disable-active_insight.sh
+run bash /volume2/docker/ebertek-compose/Scripts/startup/50-sdp.sh
+run bash /volume2/docker/ebertek-compose/Scripts/startup/60-rclone.sh
+run bash /volume2/docker/ebertek-compose/Scripts/startup/70-youtube.sh
+if cd /volume2/docker/_backup/; then run bash /volume2/docker/synology-docker/syno_docker_update.sh update -f -p /volume2/docker/_backup; fi
+run bash /volume1/homes/Hannibal/Synology_HDD_db-main/syno_hdd_db.sh -nr
