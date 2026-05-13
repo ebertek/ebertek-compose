f7b284a8b510eb1cbdfcf49af9314a32373b3dbf
eebedav <ebertek@gmail.com>
2026-05-13 16:25:59 +0200

    Add --delete

--- Scripts/backup/hc-sync.sh
+++ Scripts/backup/hc-sync.sh
@@ -16,7 +16,7 @@ while IFS='=' read -r key value; do
 done <"$ENV_FILE"
 
 echo Backing up to Yggdrasil...
-rsync -e "ssh -p $PORT" -av --exclude docker/ /mnt/data/ "$USER@$HOST:/volume1/NetBackup/backupdata/"
+rsync -e "ssh -p $PORT" -av --delete --exclude docker/ /mnt/data/ "$USER@$HOST:/volume1/NetBackup/backupdata/"
 echo Backing up to Hetzner...
 /usr/bin/rclone sync --bwlimit 10M --config=/root/ygg-compose/Scripts/backup/rclone.conf --fast-list --filter-from /root/ygg-compose/Scripts/backup/rclone-filter.txt --links --local-no-check-updated /mnt/data/ storagebox:data
 echo Done!
